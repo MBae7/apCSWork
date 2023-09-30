5c76810afc36be30f4cbc97e49eea99d543b3c9f
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-09-29 22:56:37 -0700

    dice

---
+++ unit3/Dice.java
@@ -0,0 +1,9 @@
+public class Dice{
+    public static void main(String[] args){
+      for(int i=0; i<20; i++){
+        int n = (int)(Math.random()*6)+1;
+            System.out.println(n);
+      }
+    }
+        
+}
