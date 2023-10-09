4bcbc04e15cfd1892c529f4c591e8ae81b0ffad2
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-10-09 14:40:44 -0700

    reverse

---
+++ unit4/Reverse.java
@@ -0,0 +1,20 @@
+public class Reverse{
+    public static String reverse(String str){
+        String x = "";
+        for(int i=0; i<str.length();i++){
+           String h = str.substring(str.length()-i-1,str.length()-i);
+            x+=h;
+        }
+        System.out.println(x);
+        return x;
+    }
+    
+    public static void main(String args[]){
+        reverse("happy");
+        reverse("potato");
+        reverse("tacocat");
+        reverse("Hello, world!");
+        reverse("I am a frog");
+    }
+    
+}
