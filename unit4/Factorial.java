789f8e6e4909208c171367b92a61785bd3953006
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-10-13 14:29:33 -0700

    factorial test

--- unit4/Factorial.java
+++ unit4/Factorial.java
@@ -3,21 +3,34 @@ public class Factorial{
             long b =1;
         for(int i=1; i<a+1; i++){
             b*=i;
-            System.out.println(b);
-             System.out.println(i);
+           // System.out.println(b);
+            // System.out.println(i);
         }
-        System.out.println(b);
+        //System.out.println(b);
         return(b);
     }
     
+    public static void testFactorial(int a, int expected){
+        long result = factorial(a);
+        
+        System.out.println("original int = "+a+ " expected = "+expected+" result = "+result);
+        
+        if (result == expected){
+            System.out.println("Yay!!!!!!");
+        }else{
+            System.out.println("BOOOOOOO");
+        }
+        
+    }
+    
     public static void main(String[] args){
-        factorial(0);
-        factorial(1);
-        factorial(2);
-        factorial(3);
-        factorial(4);
-        factorial(5);
-        factorial(10);
-        factorial(18);
+        testFactorial(0,1);
+        testFactorial(1,1);
+        testFactorial(2,2);
+        testFactorial(3,6);
+        testFactorial(4,24);
+        testFactorial(5,120);
+        testFactorial(10,3628800);
+       // testFactorial(18);
     }
 }
