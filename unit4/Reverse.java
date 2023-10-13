a250b205d62476a9f23d7a9d2cbbbaf903dfb561
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-10-13 14:44:21 -0700

    reverse test

--- unit4/Reverse.java
+++ unit4/Reverse.java
@@ -5,18 +5,30 @@ public class Reverse{
            String h = str.substring(str.length()-i-1,str.length()-i);
             x+=h;
         }
-        System.out.println(x);
+     
         return x;
     }
     
+    public static void testReverse(String str, String expected){
+        String result = reverse(str);
+        
+         System.out.println("String = "+str+ " expected = "+expected+" result = "+result);
+        
+        if (result.equals(expected)){
+            System.out.println("Yay!!!!!!");
+        }else{
+            System.out.println("BOOOOOOO");
+        }
+}
+    
     public static void main(String args[]){
-        reverse("happy");
-        reverse("potato");
-        reverse("tacocat");
-        reverse("Hello, world!");
-        reverse("I am a frog");
-         reverse("Jessica");
-        reverse("cara huang");
+        testReverse("happy", "yppah");
+        testReverse("potato", "otatop");
+        testReverse("tacocat", "tacocat");
+        testReverse("Hello, world!", "!dlrow ,olleH");
+        testReverse("I am a frog", "gorf a ma I");
+        testReverse("Jessica", "acisseJ");
+        testReverse("cara huang", "gnauh arac");
     }
     
 }
