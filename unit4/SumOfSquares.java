1ef1fb3ecbd3d2e85a2c96aad6380f37ce3a6c64
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-10-13 21:01:31 -0700

    SumOfSquares test

--- unit4/SumOfSquares.java
+++ unit4/SumOfSquares.java
@@ -8,11 +8,25 @@ public class SumOfSquares{
      System.out.println (x);
      return x;
     }
+    
+    public static void testSumOfSquares(int a, int expected){
+        int result = sumOfSquares(a);
+        
+        System.out.println("original int = "+a+ " expected = "+expected+" result = "+result);
+        
+        if (result == expected){
+            System.out.println("Yay!!!!!!");
+        }else{
+            System.out.println("BOOOOOOO");
+        }
+        
+    }
+    
     public static void main(String[] args){
-       sumOfSquares(1);
-       sumOfSquares(2);
-       sumOfSquares(3);
-       sumOfSquares(4);
-       sumOfSquares(5);
+       testSumOfSquares(1,1);
+       testSumOfSquares(2,5);
+       testSumOfSquares(3,14);
+       testSumOfSquares(4,30);
+       testSumOfSquares(5,55);
     }
 }
