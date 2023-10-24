3b22e21cc66be60fd7302b2739a51469e0f3ebd4
MBae7 <143737119+MBae7@users.noreply.github.com>
2023-10-24 09:00:08 -0700

    bouncingball

--- unit6/BouncingBall.java
+++ unit6/BouncingBall.java
@@ -1,18 +1,22 @@
 import processing.core.*;
-public class BouncingBall extends PApplet {
+public class BouncingBall {
+  private PApplet p;
   private float x,y;
   //velocity 
   private float xv,yv;
   //optionally color and size 
   //private color c;
   private float s;
+  private float c;
     
-public BouncingBall(float x, float y, float xv, float vy, float s){
+public BouncingBall(PApplet p, float x, float y, float xv, float yv, float s, float c){
+        this.p = p;
         this.x = x;
         this.y = y;
         this.xv = xv;
         this.yv = yv;
         this.s = s;
+        this.c = c;
     }
     
  public void update(){
@@ -22,18 +26,18 @@ public BouncingBall(float x, float y, float xv, float vy, float s){
   }
   //check edges
  public void checkEdges(){
- if (x>width || x<0){
+ if (x>p.width || x<0){
    xv = -1*xv;
-   fill(random(255),random(255),random(255));
- }else if (y>height || y<0){
+   p.fill(p.random(255),p.random(255),p.random(255));
+ }else if (y>p.height || y<0){
    yv = -1*yv;
-      fill(random(255),random(255),random(255));
+      p.fill(p.random(255),p.random(255),p.random(255));
  }
   }
   //display the ball
  public void display(){
-   fill(random(255),random(255),random(255));
-   ellipse(x,y,s,s); 
+   p.fill(x+c,y*c/4,(x+y)/c*2);
+   p.ellipse(x,y,s,s); 
   }
   
   
@@ -41,9 +45,4 @@ public BouncingBall(float x, float y, float xv, float vy, float s){
 
 
 
-    
-    public static void main(String[] args)
-    {
-        PApplet.main("BouncingBall");
-    }
 }
--- unit6/BouncingBallTest.java
+++ unit6/BouncingBallTest.java
@@ -1,16 +1,20 @@
 import processing.core.*;
 public class BouncingBallTest extends PApplet {
-     BouncingBall b = new BouncingBall(200,100,1,3,20);
-     BouncingBall b2 = new BouncingBall(100,150,2,2,40);
-     BouncingBall b3 = new BouncingBall(300,300,3,1,30);
+     BouncingBall b = new BouncingBall(this,200,100,1,3,20,20);
+     BouncingBall b2 = new BouncingBall(this,100,150,2,2,40,3);
+     BouncingBall b3 = new BouncingBall(this,300,300,3,1,30,5);
+    
+   public void settings(){
+        size(400,400);
+   }
     
     public void setup(){
-        size(400,400);
+       
        
         
     }
     public void draw(){
-      background(250);  
+      background(0);  
       b.update();
       b.checkEdges();
       b.display();
